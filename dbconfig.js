ba68c5a6c15ae43ee1aa147549018303697ef7cd
Le Minh Nhut <leminhnhutcto@gmail.com>
2021-12-31 01:19:59 +0700

    code

---
+++ dbconfig.js
@@ -0,0 +1,15 @@
+
+const config = {
+    user :'sa',
+    password :'Sa123456',
+    server:'localhost',
+    database:'test',
+    options:{
+        trustedconnection: true,
+        enableArithAbort : true, 
+        instancename :'MSSQLSERVER'
+    },
+    port : 1433
+}
+
+module.exports = config; 
